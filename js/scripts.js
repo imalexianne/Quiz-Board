65bcde64d56d663419965164dfbf7ade79da97ce
Imanirakarama Alexianne <imanirax@yahoo.fr>
2018-12-21 11:36:22 +0200

    add my var q3 as a place for the question 3 answer

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,4 @@ function mytest()
 {
     var q1=document.forms["myform"]["answer1"].value;
     var q2=document.forms["myform"]["answer2"].value;
+    var q3=document.forms["myform"]["answer3"].value;
