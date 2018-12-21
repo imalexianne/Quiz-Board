15b9432cfe26459f1f61581d95f40981331704f8
Imanirakarama Alexianne <imanirax@yahoo.fr>
2018-12-21 17:20:24 +0200

    add coppyright and license into the README

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,33 @@
-function mytest()
-{
-    var q1=document.forms["myform"]["answer1"].value;
-    var q2=document.forms["myform"]["answer2"].value;
-    var q3=document.forms["myform"]["answer3"].value;
-    var q4=document.forms["myform"]["answer4"].value;
-    var q5=document.forms["myform"]["answer5"].value;
-    var q6=document.forms["myform"]["answer6"].value;
-    var q7=document.forms["myform"]["answer7"].value;
-    var q8=document.forms["myform"]["answer8"].value;
-    var q9=document.forms["myform"]["answer9"].value;
-    var q10=document.forms["myform"]["answer10"].value;
+$(document).ready(function (){
+   $("#blanks form").submit(function(event){
+      var q1 = $("input:radio[name=answer1]:checked").val();
+      var q2 = $("input:radio[name=answer2]:checked").val();
+      var q3 = $("input:radio[name=answer3]:checked").val();
+       var q4 = $("input:radio[name=answer4]:checked").val();
+       
     var marks=0;
-    if(q1=="script")
+    if(q1=="right")
      {
       marks=marks+2;
    }
-   if(q2=="Hypertext markup language")
+   if(q2=="right")
 {
    marks=marks+2;
    }
-if(q3=="The head section")
-{
-   marks=marks+2;
-}
-if(q4=="Microsoft")
-{
-   marks=marks+2;
-}
-if(q5=="true")
-{
-   marks=marks+2;
-}
-if(q6=="alertBox Hello World")
-{
-   marks=marks+2;
-   }
-if(q7=="function = myFunction()")
-{
-   marks=marks+2;
-   }
-if(q8=="call myFunction()")
-{
-   marks=marks+2;
-}
-if(q9=="if i = 5")
-{
-   marks=marks+2;
-}
-if(q10==" if (i != 5)")
-{
-   marks=marks+2;
-}
-alert("your score is:"+marks);
-}
+// if(q3=="right")
+// {
+//    marks=marks+2;
+// }
+// if(q4=="right")
+// {
+//    marks=marks+2;
+// }
+
+$(".output").show();
+$("#display").text("Your score is: " + marks);
+
+event.preventDefault();
+
+
+})
+});
