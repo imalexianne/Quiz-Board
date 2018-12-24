f2a735de3206f221c5d962f58d9306ace5a951ae
imalexianne <imanirax@yahoo.fr>
2018-12-24 18:45:38 +0200

    add grids and their stylings

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
+//Interface Logic
 $(document).ready(function (){
    $("#blanks form").submit(function(event){
       var q1 = $("input:radio[name=answer1]:checked").val();
       var q2 = $("input:radio[name=answer2]:checked").val();
       var q3 = $("input:radio[name=answer3]:checked").val();
-       var q4 = $("input:radio[name=answer4]:checked").val();
-       
-    var marks=0;
-    if(q1=="right")
+      var q4 = $("input:radio[name=answer4]:checked").val();
+      var marks = [q1, q2, q3, q4];
+      var score = calculateScore(marks);
+$(".output").show();
+$("#display").text("Your score is: " + score);
+document.getElementById("quiz").style.display="block";
+$(".cont").fadeOut();
+event.preventDefault();
+})
+});
+
+// /Busines Logic
+function calculateScore(answers){
+   q1 = answers[0]
+   q2 = answers[1]
+   q3 = answers[2]
+   q4 = answers[3]
+
+   var marks = 0;
+   if(q1=="right")
      {
       marks=marks+2;
      }
@@ -22,12 +39,5 @@ if(q4=="right")
      {
   marks=marks+2;
      }
-
-$(".output").show();
-$("#display").text("Your score is: " + marks);
-
-event.preventDefault();
-
-
-})
-});
+   return marks;
+}
